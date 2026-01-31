75c897c6f7880f49958cef2d685f9a7e6b013080
Talha <175202306+kxllshot1@users.noreply.github.com>
2026-01-31 22:12:30 +0500

    completed desktop version 0.1

--- script.js
+++ script.js
@@ -8,37 +8,22 @@ const boxArticleOne = document.querySelector(".article-1");
 const boxArticleTwo = document.querySelector(".article-2");
 const boxArticleThree = document.querySelector(".article-3");
 const allBoxArticles = document.querySelectorAll(".article");
-const btnBack = document.querySelector(".btn-back");
 
 postArticleOne.addEventListener("click", function () {
   mainWindow.classList.remove("window-active");
-  btnBack.classList.add("btn-show");
   setTimeout(() => {
     boxArticleOne.classList.add("window-active");
-    btnBack.classList.add("btn-active");
   }, 1000);
 });
 postArticleTwo.addEventListener("click", function () {
   mainWindow.classList.remove("window-active");
   setTimeout(() => {
     boxArticleTwo.classList.add("window-active");
-    btnBack.classList.add("btn-active");
   }, 1000);
 });
 postArticleThree.addEventListener("click", function () {
   mainWindow.classList.remove("window-active");
   setTimeout(() => {
     boxArticleThree.classList.add("window-active");
-    btnBack.classList.add("btn-active");
-  }, 1000);
-});
-
-btnBack.addEventListener("click", function () {
-  allBoxArticles.forEach((article) => {
-    article.classList.remove("window-active");
-  });
-  btnBack.classList.remove("btn-active");
-  setTimeout(() => {
-    mainWindow.classList.add("window-active");
   }, 1000);
 });
